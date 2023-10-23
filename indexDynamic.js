11ff2ea651e920a11a398ddf01d7d509ef19c7ae
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-10-23 13:31:42 +0200

    Add navbar

--- indexDynamic.js
+++ indexDynamic.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   fs.readFile(filename, (err, data) => {
     if (err) {
       res.writeHead(404, { 'Content-Type': 'text/html' });
-      return res.end('<h1>Error: 404</h1>');
+      return res.end('<h1>Error: 404</h1><a href="/">Back Home</a>');
     }
     res.writeHead(200, { 'Content-Type': 'text/html' });
     return res.end(data);
